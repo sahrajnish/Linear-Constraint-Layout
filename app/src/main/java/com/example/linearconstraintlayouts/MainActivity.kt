c6c10f502877fecd6b2d863af6dd7e1610d4f441
RAJNISH <raj.rajnish.sah@gmail.com>
2024-01-12 07:34:32 +0530

    alpha 0.2

--- app/src/main/java/com/example/linearconstraintlayouts/MainActivity.kt
+++ app/src/main/java/com/example/linearconstraintlayouts/MainActivity.kt
@@ -11,26 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-            val firstName = findViewById<EditText>(R.id.etFirstName)
-            val lastName = findViewById<EditText>(R.id.etLastName)
-            val DOB = findViewById<EditText>(R.id.etDOB)
-            val countryName = findViewById<EditText>(R.id.etcountryName)
             val applyButton = findViewById<Button>(R.id.btApply)
-
             applyButton.setOnClickListener {
-                if(firstName.text.toString() != "" && lastName.text.toString() != "" && countryName.text.toString() != ""){
-                    val intent = Intent(this, secondActivity::class.java)
-                    intent.putExtra("FIRSTNAME", firstName.text.toString())
-                    intent.putExtra("LASTNAME", lastName.text.toString())
-                    intent.putExtra("COUNTRYNAME", countryName.text.toString())
-                    startActivity(intent)
-                }else{
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Please! enter all fields.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+                val intent = Intent(this, secondActivity::class.java)
+                startActivity(intent)
             }
     }
 }
